63fb13768160aae80fdd0b6fc68b0d966985163e
Jin <kevinniu12@gmail.com>
2025-07-16 14:47:27 -0700

    reqtime

--- myApp.js
+++ myApp.js
@@ -26,7 +26,12 @@ app.get("/json", function(req, res) {
 
   res.json({ message: message });
 });
-
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({ time: req.time });
+});
 module.exports = app;
 
 
