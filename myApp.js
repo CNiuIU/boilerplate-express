37504844bad1a477d3db0a6f1279cc8a33d94e37
Jin <kevinniu12@gmail.com>
2025-07-16 10:56:27 -0700

    app get 4

--- myApp.js
+++ myApp.js
@@ -5,7 +5,9 @@ console.log("Hello World");
 app.get("/", function(req, res) {
   res.send("Hello Express");
 });
-
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 
 
