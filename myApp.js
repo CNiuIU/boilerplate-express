25ea40ddb69595128837036932794cc29453a9a0
Jin <kevinniu12@gmail.com>
2025-07-16 14:45:29 -0700

    logger

--- myApp.js
+++ myApp.js
@@ -1,19 +1,22 @@
-require('dotenv').config(); // Load .env variables
+require('dotenv').config(); // 
 
 let express = require('express');
 let app = express();
 
 console.log("Hello World");
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
-// Serve static files if needed (optional)
 app.use('/public', express.static(__dirname + '/public'));
 
-// Root route (not part of this challenge, but commonly included)
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// ✅ /json route that changes based on MESSAGE_STYLE
+
 app.get("/json", function(req, res) {
   let message = "Hello json";
 
