2b94f4a2c372496d41edcd5878a6de171815f56f
Jin <kevinniu12@gmail.com>
2025-07-16 14:35:52 -0700

    env2

--- myApp.js
+++ myApp.js
@@ -4,17 +4,17 @@ let app = express();
 
 console.log("Hello World");
 
+// Serve static files from /public
 app.use('/public', express.static(__dirname + '/public'));
 
+// Root route now returns JSON based on MESSAGE_STYLE
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/json", function(req, res) {
   let message = "Hello json";
+  
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     message = message.toUpperCase();
   }
+
   res.json({ message: message });
 });
 
