680b5dc60ddfa25e69824d07538b01a433930bd8
Jin <kevinniu12@gmail.com>
2025-07-16 11:11:05 -0700

    7

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ let app = express();
 console.log("Hello World");
 
 
-app.get('/', function(req, res) {
+app.get(function(req, res) {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
