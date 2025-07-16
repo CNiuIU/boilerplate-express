076d642b5606f18a8b2c7b8404931bfee203a9f7
Jin <kevinniu12@gmail.com>
2025-07-16 11:30:20 -0700

    appuse7

--- myApp.js
+++ myApp.js
@@ -4,9 +4,9 @@ let app = express();
 console.log("Hello World");
 
 
-app.use(express.static(__dirname + "/public"));
+app.use('/public', express.static(__dirname + '/public'));
 
-app.get(function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
