6dfc40d4df93ec335f588b3bcf58492b3a1ff3c7
Jin <kevinniu12@gmail.com>
2025-07-17 13:55:56 -0700

    bodyparse

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 require('dotenv').config(); // 
+const bodyParser = require('body-parser');
 
 let express = require('express');
 let app = express();
 
 console.log("Hello World");
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function(req, res, next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
